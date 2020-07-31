5f1113c3dd1b2c45e7bc3277131a30811dcc7b8e
Mostfa <almme111@gmail.com>
2020-07-31 21:22:28 +0200

    Initial Commit

---
+++ BetterCombine/BetterCombine.h
@@ -0,0 +1,19 @@
+//
+//  BetterCombine.h
+//  BetterCombine
+//
+//  Created by mostfa on 7/31/20.
+//  Copyright © 2020 mostfaE. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for BetterCombine.
+FOUNDATION_EXPORT double BetterCombineVersionNumber;
+
+//! Project version string for BetterCombine.
+FOUNDATION_EXPORT const unsigned char BetterCombineVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <BetterCombine/PublicHeader.h>
+
+
